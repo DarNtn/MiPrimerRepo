20561a834d55b94c5e4109d4d574978adb5b4532
Dario Ntn Carpio <Dario Ntn Carpio@DarNtn>
2016-07-31 22:37:47 -0500

    panel organizador

---
+++ TyperShark/src/typershark/PanelOrganizador.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package typershark;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+/**
+ *
+ * @author Dario Ntn Carpio
+ */
+public class PanelOrganizador {
+    private BorderPane root;
+    
+    public PanelOrganizador(){
+        root = new BorderPane();
+        
+        
+        
+    }
+    
+    public BorderPane getRoot(){
+        return root;
+    }
+    
+    public void setSceneMenu(){
+        VBox panel = new VBox();
+        Button start = new Button("Start");
+        Button leaderBoard = new Button("LeaderBoard");
+        Button quit = new Button("Quit");
+                
+        panel.getChildren().addAll(start,leaderBoard,quit);
+        panel.setAlignment(Pos.CENTER);
+        panel.setSpacing(20);
+        
+        root.setCenter(panel);
+    }
+}
